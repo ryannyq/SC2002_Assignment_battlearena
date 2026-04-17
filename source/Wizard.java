cd6c6e14de57765ca4fe0fd69588870bcea2dbcc
ZEDWHYYY <zhengyongpok@gmail.com>
2026-04-17 21:55:59 +0800

    Update Wizard.java
    as per the example in the pdf

--- source/Wizard.java
+++ source/Wizard.java
@@ -3,8 +3,8 @@ import java.util.List;
 public class Wizard extends Combatant {
     private static final int WIZARD_HP = 200;
     private static final int WIZARD_ATK = 50;
-    private static final int WIZARD_DEF = 15;
-    private static final int WIZARD_SPD = 28;
+    private static final int WIZARD_DEF = 10;
+    private static final int WIZARD_SPD = 20;
 
     public Wizard() {
         super("Wizard", WIZARD_HP, WIZARD_ATK, WIZARD_DEF, WIZARD_SPD);
